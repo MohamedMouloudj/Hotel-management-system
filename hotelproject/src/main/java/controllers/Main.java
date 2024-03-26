fe2c4298d3c385e8f6b330a85e2328ae249123b9
MohamedMouloudj <mouloudy656565@gmail.com>
2024-03-26 22:39:20 +0100

    GuestUi

--- hotelproject/src/main/java/controllers/Main.java
+++ hotelproject/src/main/java/controllers/Main.java
@@ -1,19 +1,6 @@
 package controllers;
 
-import view.login.main.LogInForm;
-
-import java.util.*;
-
-import org.bson.Document;
-
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-
-import model.Guest;
-
-import static view.login.main.LogInForm.*;
+import view.windows.GuestUiSidebar;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,8 +8,8 @@ public class Main {
         // "john@example.com", 222);
         // Manager.addEmployee(receptionist);
         // Manager.removeEmployee("chakib", "Doe");
-        LogInForm.runForm();
-        // System.out.println(Guest.isValidEmail("uu"));
+        //LogInForm.runForm();
+        new GuestUiSidebar();
 
     }
 }
---
+++ hotelproject/src/main/java/view/components/SideLabelContainer.java
@@ -0,0 +1,69 @@
+package view.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+
+public class SideLabelContainer extends JPanel implements MouseListener {
+    private final JLabel label;
+    /**
+     * To create side tab that must be added to a container (Like tab  container)
+     * @param text tab name title.
+     *
+     * @author Mouloudj
+     *
+     * */
+    public SideLabelContainer(String text){
+        label = new JLabel();
+        label.setText(text);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Inter", Font.PLAIN, 16));
+        label.setPreferredSize(new Dimension(170, 30));
+        label.addMouseListener(this);
+
+        setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        setPreferredSize(new Dimension(170, 30));
+        setBackground(new Color(0x1E90FF));
+        add(label);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == label){
+
+            for (Component component : getParent().getComponents()){
+                component.setBackground(new Color(0x1E90FF));
+                for (Component c : ((JPanel) component).getComponents()){
+                    c.setForeground(Color.WHITE);
+                }
+            }
+            label.setForeground(Color.BLACK);
+            setBackground(Color.WHITE);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // Do nothing
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // Do nothing
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // Do nothing
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // Do nothing
+    }
+}
