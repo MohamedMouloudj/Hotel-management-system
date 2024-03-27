0929ef637fa0c8386be9d72e285e04dda524f0b8
schakib <schakibhad@gmail.com>
2024-03-27 00:33:00 +0000

    Conflict

--- hotelproject/src/main/java/controllers/Main.java
+++ hotelproject/src/main/java/controllers/Main.java
@@ -1,39 +1,20 @@
 package controllers;
 
-<<<<<<< HEAD
-import view.login.main.LogInForm;
+// import view.login.main.LogInForm;
 
-// import java.util.*;
-
-// import org.bson.Document;
-
-// import com.mongodb.MongoClient;
-// import com.mongodb.client.MongoClients;
-// import com.mongodb.client.MongoCollection;
-// import com.mongodb.client.MongoDatabase;
-
-// import model.Guest;
-
-// import static view.login.main.LogInForm.*;
-=======
 import view.windows.GuestUi;
->>>>>>> d6eda4a6e47d9e3f813247645d01f3e90afdee77
 
 public class Main {
-    public static void main(String[] args) {
-        // Receptionist receptionist = new Receptionist("karin", "Doe", "password123",
-        // "john@example.com", 222);
-        // Manager.addEmployee(receptionist);
-        // Manager.removeEmployee("chakib", "Doe");
-<<<<<<< HEAD
-        LogInForm.runForm();
+        public static void main(String[] args) {
+                // Receptionist receptionist = new Receptionist("karin", "Doe", "password123",
+                // "john@example.com", 222);
+                // Manager.addEmployee(receptionist);
+                // Manager.removeEmployee("chakib", "Doe");
 
-        // System.out.println(Guest.isValidEmail("uu"));
-=======
+                // System.out.println(Guest.isValidEmail("uu"));
 
-        //LogInForm.runForm();
-        new GuestUi();
->>>>>>> d6eda4a6e47d9e3f813247645d01f3e90afdee77
-    }
+                // LogInForm.runForm();
+                new GuestUi();
+        }
 
 }
